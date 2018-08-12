b0b6e2f315a0de64eb7327a934a01dd75941a44f
negrime <scorpion36rus@gmail.com>
2018-08-13 00:52:45 +0300

    first commit
    first commit

---
+++ TapGame/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+
+
+	public Transform target;
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+
+	private void LateUpdate()
+	{
+		transform.position = new Vector3(target.position.x + 3, target.position.y + 3, target.position.z - 10);
+	}
+}
---
+++ TapGame/Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+	private Rigidbody2D rb;
+	public float speed;
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		transform.Translate(speed * new Vector2(1,0) * Time.deltaTime, Space.World);
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(Vector2.up * 300);
+		}
+		
+	}
+
+	private void FixedUpdate()
+	{
+		//rb.velocity = speed * new Vector2(1, 0);
+	}
+}
