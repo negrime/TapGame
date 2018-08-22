205f4a2da7af513b8a7a9c1320189c87d719dedd
negrtime <scorpion36rus@gmail.com>
2018-08-23 01:19:29 +0300

    Its3.14dec

--- TapGame/Assets/Scripts/Generator.cs
+++ TapGame/Assets/Scripts/Generator.cs
@@ -30,6 +30,8 @@ public class Generator : MonoBehaviour
 		yield return new WaitForSeconds(Random.Range(4, 15));
         int rnd = Random.Range(0, 2);
         GameObject build = Instantiate(buildings[Random.Range(0, buildings.Length)], transform.position, Quaternion.identity);
+        build.transform.localScale = (new Vector2(Random.Range(2f, 4f), Random.Range(3f, 4f)));
+        //build.GetComponent<Transform>().transform.localScale = (new Vector3(1,1,1));
         build.GetComponent<SpriteRenderer>().flipX = (rnd == 1) ? true : false;
 	}
 }
